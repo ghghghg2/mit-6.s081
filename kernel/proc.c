842a47f761608901f1fbc7af1df9a6ddef61d2f2
ghghghg2 <sam7793085@gmail.com>
2024-01-23 23:55:27 +0800

    proc: map uva to kpagetable in userinit
    This is for the first proc because its uva isn't mapped to kpaetable by fork or exec.

--- kernel/proc.c
+++ kernel/proc.c
@@ -255,6 +255,8 @@ userinit(void)
   uvminit(p->pagetable, initcode, sizeof(initcode));
   p->sz = PGSIZE;
 
+  mapUvaToKva(p->pagetable, p->kPageTable, 0, p->sz);
+
   // prepare for the very first "return" from kernel to user.
   p->trapframe->epc = 0;      // user program counter
   p->trapframe->sp = PGSIZE;  // user stack pointer
