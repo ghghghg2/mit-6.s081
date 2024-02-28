7c31f43ecc27545f23ad493dfadaa787a79bb15a
ghghghg2 <sam7793085@gmail.com>
2024-02-28 22:47:59 +0800

    kfree: do not free the physical page if the refCnt isn't 0

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -73,10 +73,19 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  int tmp;
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+  
+  if ((tmp = get_pageRefCnt((uint64)pa)) > 0) {
+    // There is other process still ref to this page, 
+    // so do not free it.
+    return;
+  } else if (tmp < 0) {
+    panic("kfree: get_pageRefCnt");
+  }
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
